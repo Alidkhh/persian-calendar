bb7df88ec638eeca603c380667b73b4d80d84dc4
Ali Dadkhah <adadkhahh@gmail.com>
2025-06-29 17:27:58 +0330

    run prettier

--- pages/calendar.vue
+++ pages/calendar.vue
@@ -10,7 +10,6 @@ import {
 const value = ref(
    toCalendar(today(getLocalTimeZone()), createCalendar("persian")),
 ) as Ref<DateValue>;
-
 </script>
 
 <template>
--- registry/default/ui/tooltip/TooltipProvider.vue
+++ registry/default/ui/tooltip/TooltipProvider.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-import { TooltipProvider, type TooltipProviderProps } from 'reka-ui'
+import { TooltipProvider, type TooltipProviderProps } from "reka-ui";
 
 const props = withDefaults(defineProps<TooltipProviderProps>(), {
-  delayDuration: 500,
-})
+   delayDuration: 500,
+});
 </script>
 
 <template>
-  <TooltipProvider v-bind="props">
-    <slot />
-  </TooltipProvider>
+   <TooltipProvider v-bind="props">
+      <slot />
+   </TooltipProvider>
 </template>
