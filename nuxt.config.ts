4077f0df1c1a131bc20dffb07dbe6fe359c9c17c
Ali Dadkhah <adadkhahh@gmail.com>
2025-06-30 17:57:05 +0330

    - install nuxt/images - install scrollBar component

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
       plugins: [tailwindcss()],
    },
 
-   modules: ["@nuxt/fonts", "shadcn-nuxt"],
+   modules: ["@nuxt/fonts", "shadcn-nuxt", "@nuxt/image"],
 
    shadcn: {
       prefix: "",
