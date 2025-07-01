3172cdbd5983c829035be8fc085937c746b7ceda
Ali Dadkhah <adadkhahh@gmail.com>
2025-07-01 16:40:33 +0330

    - calendar RTL

---
+++ utils/registry.ts
@@ -0,0 +1,16 @@
+import registry from "~~/registry.json";
+import type { RegistryItem } from "shadcn-vue/registry";
+
+const components = [...registry.items] as unknown as RegistryItem[];
+
+export function getComponentsByNames(names: string[]): RegistryItem[] {
+   const componentsMap = new Map(components.map((comp) => [comp.name, comp]));
+
+   return names
+      .map((name) => componentsMap.get(name))
+      .filter((comp): comp is RegistryItem => comp !== undefined);
+}
+
+export function getComponentByName(name: string): RegistryItem | undefined {
+   return components.find((comp) => comp.name === name);
+}
