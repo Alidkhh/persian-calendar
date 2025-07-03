1e4de444c1f8bf320f2c3d8a574e973aa1476708
Ali Dadkhah <adadkhahh@gmail.com>
2025-07-03 21:55:33 +0330

    comp-5

--- utils/components.ts
+++ utils/components.ts
@@ -14,6 +14,7 @@ export const categories: ComponentCategory[] = [
          { name: "Comp-2" },
          { name: "Comp-3" },
          { name: "Comp-4" },
+         { name: "Comp-5" },
       ],
    },
 ];
