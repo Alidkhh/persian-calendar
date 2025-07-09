90587cb8c214163f2c3f30628060ab9785debfc8
Ali Dadkhah <adadkhahh@gmail.com>
2025-07-09 16:59:52 +0330

    write routes item list in a component

---
+++ components/routes-menu/Menu.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import type { HTMLAttributes } from "vue";
+import { cn } from "~/lib/utils";
+
+const props = withDefaults(
+   defineProps<{
+      class?: HTMLAttributes["class"];
+      withDrawerClose?: boolean;
+   }>(),
+   {
+      withDrawerClose: false,
+   },
+);
+
+const menuItems = [routes.introduction, routes.installation, routes.calendar];
+</script>
+
+<template>
+   <div :class="cn('flex flex-col gap-2', props.class)">
+      <DrawerClose v-if="withDrawerClose" as-child v-for="item in menuItems">
+         <RoutesMenuItem :title="item.title" :route="item.route" />
+      </DrawerClose>
+
+      <RoutesMenuItem
+         v-else
+         v-for="item in menuItems"
+         :title="item.title"
+         :route="item.route"
+      />
+   </div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,24 +1,4 @@
-<script lang="ts" setup>
-const route = useRoute();
-
-const menuItems = [
-   {
-      title: routes.introduction.title,
-      route: routes.introduction.route,
-      icon: "",
-   },
-   {
-      title: routes.installation.title,
-      route: routes.installation.route,
-      icon: "",
-   },
-   {
-      title: "Calendar Components",
-      route: routes.calendar.route,
-      icon: "",
-   },
-];
-</script>
+<script lang="ts" setup></script>
 
 <template>
    <div class="flex min-h-[100vh] flex-col">
@@ -27,22 +7,9 @@ const menuItems = [
       <LayoutBody>
          <div class="relative flex flex-1 gap-5">
             <div
-               class="border-foreground/20 sticky top-[6.5rem] hidden h-full max-h-[calc(100vh-7.5rem)] w-[15rem] flex-shrink-0 overflow-y-auto border-e font-sans text-sm md:block pe-2"
+               class="border-foreground/20 sticky top-[6.5rem] hidden h-full max-h-[calc(100vh-7.5rem)] w-[15rem] flex-shrink-0 overflow-y-auto border-e pe-2 font-sans text-sm md:block"
             >
-               <div class="flex flex-col gap-1">
-                  <NuxtLink
-                     v-for="item in menuItems"
-                     :to="item.route"
-                     class="hover:bg-muted-foreground/30 text-md w-full cursor-pointer justify-start rounded-lg px-2.5 py-1.5"
-                     :class="
-                        route.fullPath == item.route
-                           ? 'bg-muted-foreground/40 hover:bg-muted-foreground/40 font-bold'
-                           : ''
-                     "
-                  >
-                     {{ item.title }}
-                  </NuxtLink>
-               </div>
+               <RoutesMenu />
             </div>
             <div class="flex-1">
                <slot></slot>
