2cbb62cb40b6bda64abf3306befa144918b517d0
Ali Dadkhah <adadkhahh@gmail.com>
2025-07-13 02:01:28 +0330

    page transition

--- app.vue
+++ app.vue
@@ -9,3 +9,14 @@ import { SpeedInsights } from "@vercel/speed-insights/nuxt";
       </NuxtLayout>
    </div>
 </template>
+<style>
+.page-enter-active,
+.page-leave-active {
+   transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+   opacity: 0;
+   transform: translate(0, 80px);
+}
+</style>
